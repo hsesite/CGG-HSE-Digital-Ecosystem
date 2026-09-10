6112619b4020f3bd307a53a5ed28f92a4a863276
hsesite <hse.site@ptcgg.com>
2026-09-11 03:32:45 +0800

    Remove call to initializeDockMagnification

--- js/sidebar.js
+++ js/sidebar.js
@@ -70,7 +70,6 @@ function refreshNavigation() {
 function initializeNavigation() {
   refreshNavigation();
   bindNavigationEvents();
-  initializeDockMagnification();
 }
 
 /* =========================================
