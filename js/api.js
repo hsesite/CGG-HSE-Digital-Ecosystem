72e244e9225a9ba5ff09f8f0400e1a48b92ec443
hsesite <hse.site@ptcgg.com>
2026-09-11 06:06:37 +0800

    Update API_URL and refactor apiGet/apiPost functions

--- js/api.js
+++ js/api.js
@@ -3,36 +3,24 @@
    CGG HSE API Client v1.0
    ========================================== */
 
-const API_URL = "PASTE_WEB_APP_URL_DISINI";
+const API_URL = "https://script.google.com/macros/s/AKfycbxI1I0jxW14JY_H4gwqoVYkxdpCY635lm-LAPZVdh0-zwN9vK_yalQSLjAFkiho6Tkp9g/exec";
 
-async function apiGet(action){
-
+async function apiGet(action) {
   const res = await fetch(`${API_URL}?action=${action}`);
-
   return await res.json();
-
 }
 
-async function apiPost(action,payload){
-
-  const res = await fetch(API_URL,{
-
-    method:"POST",
-
-    headers:{
-      "Content-Type":"application/json"
+async function apiPost(action, payload) {
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
     },
-
-    body:JSON.stringify({
-
+    body: JSON.stringify({
       action,
-
       ...payload
-
     })
-
   });
 
   return await res.json();
-
 }
