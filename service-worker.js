dc1eeb84b42956a7d82cdfd532cda9651ea554c4
hsesite <hse.site@ptcgg.com>
2026-09-15 11:22:28 +0800

    Implement service worker install and activate events
    Add install and activate event listeners for service worker.

--- service-worker.js
+++ service-worker.js
@@ -44,3 +44,28 @@ self.addEventListener("fetch", event => {
   );
 
 });
+self.addEventListener("install", event=>{
+
+self.skipWaiting();
+
+});
+
+self.addEventListener("activate", event=>{
+
+event.waitUntil((async()=>{
+
+const keys=await caches.keys();
+
+await Promise.all(
+
+keys
+.filter(k=>k!==CACHE_NAME)
+.map(k=>caches.delete(k))
+
+);
+
+await self.clients.claim();
+
+})());
+
+});
