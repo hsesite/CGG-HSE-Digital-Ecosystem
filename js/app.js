c0434d7e25bbf9c32bfe605bafb2dffb173fd407
hsesite <hse.site@ptcgg.com>
2026-09-19 15:14:10 +0800

    Initialize HDOSUpload if available

--- js/app.js
+++ js/app.js
@@ -24,6 +24,11 @@ async function fastBoot(){
             console.log("✓ Router Ready");
         }
 
+       if(window.HDOSUpload){
+            HDOSUpload.init();
+            console.log("✓ Upload Launcher Ready");
+        }
+
         document.getElementById("splash-screen")
             ?.classList.add("fade-out");
 
